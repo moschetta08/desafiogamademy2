02f5a8850a40c0c7b55b96dc48ce941dd2bd4d59
moschetta08 <60576590+moschetta08@users.noreply.github.com>
2021-07-25 17:46:10 -0300

    syle from home

---
+++ style.ts
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width: 80%;
+    margin: auto;
+
+    .nav{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+    .shopcart{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+    }
+    }
+
+    section {
+        height: auto;
+        width: 100%;
+        background: yellow;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+        border-radius: 12px;
+
+    .product-content{
+        display: grid;
+        text-align: center;
+        height: 300px;
+        background: pink;
+        border-radius: 12px;
+        padding: 12px;
+        margin: 12px;
+    }
+    }
+`;
+
+
